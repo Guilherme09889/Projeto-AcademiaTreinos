36f36374483cce8a34c9b3835c6283ba6e2333bd
Guilherme Kochhann Reisdorfer <guilherme.kochhann9@gmail.com>
2026-07-28 14:19:30 -0300

    Construção do UsuarioEntity e AvaliacaoFisicaEntity

---
+++ demo/src/main/java/com/example/demo/model/entity/AvaliacaoFisicaEntity.java
@@ -0,0 +1,36 @@
+package com.example.demo.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "avaliacoes_fisicas")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AvaliacaoFisicaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Double peso;
+
+    @Column(nullable = false)
+    private Double altura;
+
+    @Column(nullable = false)
+    private LocalDateTime dataAvaliacao;
+
+}
